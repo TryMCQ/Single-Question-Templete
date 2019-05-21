a3a2e7364876a5932fd72965db77722a41a5e95f
TryMCQ <50855258+TryMCQ@users.noreply.github.com>
2019-05-21 08:40:09 +0530

    Create Options onclick.js

---
+++ Options onclick.js
@@ -0,0 +1,60 @@
+
+    // This is the Javascript file which contains the action taken on the button click.
+    // These function will be launched onclick of the option button
+
+    // Function for the First Option
+    function rans1(){
+        document.getElementById("sbo1").innerHTML = 
+            " <font size= 6>  &#9989; </font> ";
+        document.getElementById("ob1").style.color = "green";
+        document.getElementById("moreinfo").style.display = "block";
+    }
+    function wans1(){
+        document.getElementById("sbo1").innerHTML = 
+                    " <font size= 6>  &#10060; </font> ";
+        document.getElementById("ob1").style.color = "Red";
+        document.getElementById("moreinfo").style.display = "block";
+    }
+
+                    // Function for the Second Option
+                    function rans2(){
+                        document.getElementById("sbo2").innerHTML = 
+                            " <font size= 6>  &#9989; </font> ";
+                        document.getElementById("ob2").style.color = "green";
+                        document.getElementById("moreinfo").style.display = "block";
+                            }
+                    function wans2(){
+                        document.getElementById("sbo2").innerHTML = 
+                                    " <font size= 6>  &#10060; </font> ";
+                        document.getElementById("ob2").style.color = "Red";
+                        document.getElementById("moreinfo").style.display = "block";
+                                    }
+                
+    // Function for the Third option
+    function rans3(){
+        document.getElementById("sbo3").innerHTML = 
+            " <font size= 6>  &#9989; </font> ";
+        document.getElementById("ob3").style.color = "green";
+        document.getElementById("moreinfo").style.display = "block";
+            }
+    function wans3(){
+        document.getElementById("sbo3").innerHTML = 
+                    " <font size= 6>  &#10060; </font> ";
+        document.getElementById("ob3").style.color = "Red";
+        document.getElementById("moreinfo").style.display = "block";
+                    }
+
+                    // Function for the Fourth Option
+                    function rans4(){
+                        document.getElementById("sbo4").innerHTML = 
+                            " <font size= 6>  &#9989; </font> ";
+                        document.getElementById("ob4").style.color = "green";
+                        document.getElementById("moreinfo").style.display = "block";
+                            }
+                    function wans4(){
+                        document.getElementById("sbo4").innerHTML = 
+                                    " <font size= 6>  &#10060; </font> ";
+                        document.getElementById("ob4").style.color = "Red";
+                        document.getElementById("moreinfo").style.display = "block";
+                                    }
+            
